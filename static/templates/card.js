9ce32f09b391e6a4bf1717bc1ff2bf97be8d4709
mbellew <matthew@bellew.net>
2015-06-23 08:39:54 -0700

    new reactjs page

--- static/templates/card.js
+++ static/templates/card.js
@@ -7,13 +7,22 @@ var SocialCard = React.createClass(
             return React.createElement("li", null, React.createElement("a", {href: link.href}, link.text))
         };
 
-        return React.createElement("div", {className: "card"}, 
-            React.createElement("h3", null, this.props.name), 
-            React.createElement("table", null, React.createElement("tr", null, React.createElement("td", null, 
-                React.createElement("img", {width: "150", height: "150", src: this.props.img})
-            ), React.createElement("td", null, 
-                React.createElement("ul", null, this.props.links.map(renderLink))
-            )))
+        var cardStyle =
+        {
+            "background-color":"#eeeeee;",
+            "padding":"15pt",
+            "margin":"15pt"
+        };
+
+        return (
+            React.createElement("div", {style: cardStyle}, 
+                React.createElement("table", null, React.createElement("tr", null, React.createElement("td", null, 
+                    React.createElement("img", {width: "150", height: "150", src: this.props.img})
+                ), React.createElement("td", {style: {"vertical-align":"top"}}, 
+                    React.createElement("font", {style: {"font-family":"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;", "font-size":"24pt", "font-weight":"500", "padding-left":"20px"}}, this.props.name), 
+                    React.createElement("ul", {style: {"list-style-type": "none"}}, this.props.links.map(renderLink))
+                )))
+            )
         );
     }
 });
--- static/templates/card.jsx
+++ static/templates/card.jsx
@@ -7,13 +7,22 @@ var SocialCard = React.createClass(
             return <li><a href={link.href}>{link.text}</a></li>
         };
 
-        return <div className="card">
-            <h3>{this.props.name}</h3>
-            <table><tr><td>
-                <img width="150" height="150" src={this.props.img} />
-            </td><td>
-                <ul>{this.props.links.map(renderLink)}</ul>
-            </td></tr></table>
-        </div>;
+        var cardStyle =
+        {
+            "background-color":"#eeeeee;",
+            "padding":"15pt",
+            "margin":"15pt"
+        };
+
+        return (
+            <div style={cardStyle}>
+                <table><tr><td>
+                    <img width="150" height="150" src={this.props.img} />
+                </td><td style={{"vertical-align":"top"}}>
+                    <font style={{"font-family":"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;", "font-size":"24pt", "font-weight":"500", "padding-left":"20px"}}>{this.props.name}</font>
+                    <ul style={{"list-style-type": "none"}}>{this.props.links.map(renderLink)}</ul>
+                </td></tr></table>
+            </div>
+        );
     }
 });
