e77a83243c1e33ceaf0d26125f64cbe62aeff66a
mbellew <matthew@bellew.net>
2015-06-24 13:42:09 -0700

    Cache-Control

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 var http = require("http");
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
-var serve = serveStatic("./", { 'index': ['static/index.html'] });
+var serve = serveStatic("./", { 'index': ['static/index.html'], setHeaders: setCustomCacheControl });
 http.createServer(function (request, response) {
     var url = request.url;
     if (url.match(".*\\.api")) {
         var name = url.substring(url.lastIndexOf("/") + 1, request.url.length - 4);
         if (name in apis) {
             var ret = apis[name](request, response);
+            response.setHeader('Cache-Control', 'private, max-age=0, no-cache');
             response.write(JSON.stringify(ret));
             response.end();
             return;
@@ -16,6 +17,9 @@ http.createServer(function (request, response) {
     }
     serve(request, response, finalhandler(request, response));
 }).listen(process.env.PORT || 8081);
+function setCustomCacheControl(response, path) {
+    response.setHeader('Cache-Control', 'public, max-age=86400');
+}
 var apis = {
     text: function (request, response) {
         return { success: true };
--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ var http = require("http");
 
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
-var serve = serveStatic("./",{'index': ['static/index.html']});
+var serve = serveStatic("./",{'index': ['static/index.html'], setHeaders:setCustomCacheControl});
 
 http.createServer(function(request, response)
 {
@@ -15,6 +15,7 @@ http.createServer(function(request, response)
         if (name in apis)
         {
             var ret = apis[name](request, response);
+            response.setHeader('Cache-Control', 'private, max-age=0, no-cache');
             response.write(JSON.stringify(ret));
             response.end();
             return;
@@ -23,6 +24,13 @@ http.createServer(function(request, response)
 
     serve(request, response, finalhandler(request, response));
 }).listen(process.env.PORT || 8081);
+
+
+function setCustomCacheControl(response, path)
+{
+    response.setHeader('Cache-Control', 'public, max-age=86400');
+}
+
 
 var apis =
 {
