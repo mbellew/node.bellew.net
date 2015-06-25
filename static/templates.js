a816503ef7039052c44e6dee2c876bbe5548de88
mbellew <matthew@bellew.net>
2015-06-24 21:44:32 -0700

    react-templates

--- static/templates.js
+++ static/templates.js
@@ -26,3 +26,30 @@ var SocialCard = React.createClass(
         );
     }
 });
+var cardRT = function () {
+    function repeatLink1(link, linkIndex) {
+        return React.createElement('li', {}, React.createElement('a', { 'href': link.href }, link.text));
+    }
+    return React.createElement('div', {
+        'style': {
+            backgroundColor: '#eeeeee',
+            padding: '15pt',
+            margin: '15pt'
+        }
+    }, React.createElement('table', {}, React.createElement('tr', {}, React.createElement('td', {}, React.createElement('img', {
+        'width': '150',
+        'height': '150',
+        'src': this.props.img
+    })), React.createElement('td', { 'style': { verticalAlign: 'top' } }, React.createElement('div', {
+        'style': {
+            fontFamily: '\'Helvetica Neue\', Helvetica, Arial, sans-serif',
+            fontSize: '24pt',
+            fontWeight: '500',
+            paddingLeft: '20pt'
+        }
+    }, this.props.name), React.createElement.apply(this, [
+        'ul',
+        { 'style': { listStyleType: 'none' } },
+        _.map(this.props.links, repeatLink1.bind(this))
+    ])))));
+};
