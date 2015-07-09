feeeabefe2427654449ca4930458f855c015a836
mbellew <matthew@bellew.net>
2015-07-09 15:32:56 -0700

    node -> nodejs

--- static/bellew.js
+++ static/bellew.js
@@ -31,3 +31,30 @@ $(document).ready(function () {
     if (!location.hash)
         location.hash = "#social";
 });
+var cardRT = function () {
+    function repeatLink1(link, linkIndex) {
+        return React.createElement('li', { 'key': link.key }, React.createElement('a', { 'href': link.href }, link.text || link.key));
+    }
+    return React.createElement('div', {
+        'style': {
+            backgroundColor: '#eeeeee',
+            padding: '15pt',
+            margin: '15pt'
+        }
+    }, React.createElement('table', {}, React.createElement('tr', {}, React.createElement('td', {}, React.createElement('img', {
+        'width': '150',
+        'height': '150',
+        'src': this.props.img
+    })), React.createElement('td', { 'style': { verticalAlign: 'top' } }, React.createElement('div', {
+        'style': {
+            fontFamily: '\'Helvetica Neue\', Helvetica, Arial, sans-serif',
+            fontSize: '24pt',
+            fontWeight: '500',
+            paddingLeft: '20pt'
+        }
+    }, this.props.name), React.createElement.apply(this, [
+        'ul',
+        { 'style': { listStyleType: 'none' } },
+        _.map(this.props.links, repeatLink1.bind(this))
+    ])))));
+};
