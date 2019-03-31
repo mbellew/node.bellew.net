3da47c1ab954135bc9e3bafb683d7e9b1337de98
Ubuntu <ubuntu@ip-10-4-0-243.us-west-2.compute.internal>
2019-03-31 22:33:35 +0000

    risd.html

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -4,7 +4,9 @@ var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 var serveWWW = serveStatic("./www",{'index': ['static/index.html'], setHeaders:setCustomCacheControl});
 
-var port = process.env.PORT || 8080;
+var port:number = 8080;
+if (process.env.PORT)
+    port = parseInt(process.env.PORT);
 
 console.log("listening on port " + port);
 
@@ -45,3 +47,68 @@ var apis =
         return {success:true, message:'hello'};
     }
 };
+
+
+
+
+var ws = require("nodejs-websocket")
+
+var ROWS:number = 25;
+var COLS:number = 80;
+
+var strings = [];
+for (var i=0 ; i<ROWS ; i++)
+    strings[i] = "                                                                                                        ";
+ 
+var server = ws.createServer(function (conn) 
+{
+    try
+    {
+		//refresh(conn);
+    }
+    catch (err)
+    {
+    }
+	
+    conn.on("text", function (str) 
+    {
+        try
+        {
+	    if (str=="refresh")
+            {  
+		refresh(conn); 
+		return;
+	    }
+	    var change = JSON.parse(str); 
+	    if (change.row >= 0 && change.row < ROWS && change.col >=0 && change.col < COLS && change.text) 
+	    {
+                strings[change.row] = strings[change.row].substring(0,change.col) + change.text + strings[change.row].substring(change.col+change.text.length);
+                strings[change.row] = strings[change.row].substring(0,COLS);
+	        broadcast(server, change);
+	    }
+	}
+	catch (err) 
+	{
+		//err.message;
+	}    
+    });
+}).listen(port+1);
+
+function broadcast(server, change) 
+{
+    var msg = JSON.stringify(change);
+    server.connections.forEach(function (conn) {
+        console.log("SEND : " + msg);
+        conn.sendText(msg)
+    })
+}
+
+
+function refresh(conn)
+{
+    for (var i=0 ; i<ROWS ; i++)
+    {
+        conn.sendText(JSON.stringify({row:i, col:0, text:strings[i]}));
+    }
+}
+
---
+++ src/www/risd.js
@@ -0,0 +1,163 @@
+var ROWS=25, COLS=80;
+var CharData = function(row, col)
+{
+	this.el = null;
+	this.row = row;
+	this.col = col;
+	this.id = row + ":" + col;
+	this.char = " ";
+	this.opacity=1.0;
+}
+CharData.prototype = 
+{
+	update : function()
+	{
+	    this.el = this.el || document.getElementById(this.id);
+		if (this.el)
+		{
+			if (this.char == ' ')
+				this.el.innerHTML = '&nbsp;';
+			else
+				this.el.innerText = this.char;
+			this.el.style.opacity = this.opacity;
+			var selected = (this.row == State.cursor.row && this.col == State.cursor.col);
+			this.el.className = selected ? "selected" : "";
+		}
+	},
+	fade : function()
+	{
+		if (this.char == ' ')
+		  return;
+		this.opacity = this.opacity*0.99 - 0.005;
+		if (this.opacity <= 0)
+		    this.char == ' ';
+		this.update();
+	}
+};
+var State;
+var ws;
+
+
+function initState()
+{
+	State = {cursor: {row:Math.floor(Math.random()*20), col:Math.floor(Math.random()*60)}, chars:[]};
+	for (var r=0 ; r<ROWS ; r++) 
+	{
+		State.chars[r] = [];
+		for (var c=0; c<COLS ; c++) 
+			State.chars[r][c] = new CharData(r,c);
+	}
+}
+function isprint(char) 
+{
+     return !( /[\x00-\x08\x0E-\x1F\x80-\xFF]/.test(char));
+}
+function onKeyPress()
+{
+	console.log("KEY:" + event.key);
+	var prev_cursor = {row:State.cursor.row, col:State.cursor.col };
+	if (event.key.length == 1)
+	{
+		var char = event.key; // String.fromCharCode(event.keyCode);
+		if (isprint(char))
+		{
+			ws.send(JSON.stringify({row:State.cursor.row, col:State.cursor.col, text:char}));
+			State.cursor.col += 1;
+			if (State.cursor.col >= COLS)
+				State.cursor.row+=1;
+		}
+		else
+		{
+			console.log(event.keyCode);
+		}
+	}
+	else if (event.key == "ArrowUp")
+		State.cursor.row -= 1;
+	else if (event.key == "ArrowDown")
+		State.cursor.row += 1;
+	else if (event.key == "ArrowRight")
+		State.cursor.col += 1;
+	else if (event.key == "ArrowLeft")
+		State.cursor.col -= 1;
+	else if (event.key == "Backspace")
+		State.cursor.col -= 1;
+	else if (event.key == "Enter")
+	{
+		State.cursor.col = 0;
+		State.cursor.row += 1;
+	}
+	if (State.cursor.row >= ROWS)
+		State.cursor.row -= ROWS;
+	else if (State.cursor.row < 0)
+		State.cursor.row += ROWS;
+	if (State.cursor.col >= COLS)
+		State.cursor.col -= COLS;
+	else if (State.cursor.col < 0)
+		State.cursor.col += COLS;
+
+    State.chars[State.cursor.row][State.cursor.col].update();
+	State.chars[prev_cursor.row][prev_cursor.col].update();
+}
+function onLoad()
+{
+	initState();
+	document.body.onkeydown=onKeyPress;
+	drawApp();
+	fadeAll();
+}
+function fadeAll()
+{
+	for (var r=0 ; r<ROWS; r++)
+	for (var c=0 ; c<COLS ; c++)
+	    State.chars[r][c].fade();
+	window.setTimeout(fadeAll,500);
+}
+function drawApp()
+{
+	var el = document.getElementById("app");
+	var html = [];
+	html.push('<table class="app" cellspacing=0>');
+	for (var r=0 ; r<ROWS; r++)
+	{
+		html.push("<tr>");
+		for (var c=0 ; c<COLS ; c++)
+		{
+			var ch = State.chars[r][c].char;
+			var opacity = State.chars[r][c].opacity;
+			html.push('<td style="opacity:' + opacity + ';" id=\"' + r + ":" + c + '\">' + (ch==' '?'&nbsp;':ch)+'</td>');
+		}
+		html.push("</tr>");
+	}
+	html.push("</table>");
+	el.innerHTML=html.join('');
+	setupWebSocket();
+}
+function setupWebSocket()
+{		
+	ws = new WebSocket("ws://localhost:8001/")
+	ws.onmessage = function (evt) 
+	{ 
+		var change = JSON.parse(evt.data);
+		if (change.row >= 0 && change.row < ROWS && change.col >=0 && change.col <= COLS && change.text)
+		{
+			var len = change.text.length;
+			for (var i=0 ; i<len ; i++)
+			{
+				var to = change.col + i;
+				if (to < COLS)
+				{
+					var char = State.chars[change.row][to];
+					char.char = change.text[i];
+					char.opacity=1.0;
+					char.update();
+				}
+			}
+		}
+	};
+	ws.onopen = function() 
+	{
+//		ws.send("refresh"); 
+	};
+}
+
+window.onload=onLoad;
